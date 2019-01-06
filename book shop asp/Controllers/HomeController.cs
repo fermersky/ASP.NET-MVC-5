e720527df1dde2029ff0e92ac44a7c77c5659f08
Daniel Skrypnik <forte201253@gmail.com>
2019-01-06 13:34:58 +0200

    Added project files

---
+++ book shop asp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace book_shop_asp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BookShopContext db = new BookShopContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Books()
+        {
+            ViewBag.Books = db.Books;
+            return View();
+        }
+    }
+}
---
+++ book shop asp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="book_shop_asp.MvcApplication" Language="C#" %>
